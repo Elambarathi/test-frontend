693aafdd3ae4faa86db3b3e9059fdd08452c5154
Elambarathi k <156106606+Elambarathi@users.noreply.github.com>
2024-09-12 02:04:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ProductAddPage from './ProductAddPage';
 import ProductListPage from './ProductListPage';
 
 function App() {
   return (
     <Router>
-      <Switch>
+      <Routes>
         <Route path="/add-product" component={ProductAddPage} />
         <Route path="/" component={ProductListPage} />
-      </Switch>
+      </Routes>
     </Router>
   );
 }
